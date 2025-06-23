f1662fb447dec37fc226708b346d27a29301f693
Adarshswagh <waghmareadarsh1432@gmail.com>
2025-06-23 15:38:54 +0530

    new changes

---
+++ vendor/silviolleite/laravelpwa/resources/views/offline.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h1>You're Offline</h1>
+        <p>This app requires an internet connection.</p>
+    </div>
+@endsection
